5ab1104396cf29447bcc39431d26cbec2362e011
ReyLouis <louis.rey95@gmail.com>
2019-12-12 14:49:47 +0100

    Classe Magasin et produit

--- src/ecommerce/Magasin.java
+++ src/ecommerce/Magasin.java
@@ -1,11 +1,15 @@
 package ecommerce;
 
+import java.util.ArrayList;
+
 public class Magasin {
 
+	public ArrayList<Produit> produit;
+	public String nom;
 	
-	
-	public Magasin()
+	public Magasin(ArrayList<Produit> produit, String nom)
 	{
-		
+		this.produit = produit;
+		this.nom = nom;
 	}
 }
---
+++ src/ecommerce/Produit.java
@@ -0,0 +1,13 @@
+package ecommerce;
+
+public class Produit {
+
+	public double prix;
+	public String nom;
+	
+	public Produit(double prix, String nom) {
+		this.prix = prix;
+		this.nom = nom;
+	}
+
+}
